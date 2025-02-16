bbdcf68dd2ce8b4f0bce6fbfb074132745b5d8e7
Joskemu <vadhelkemu098@gmail.com>
2025-02-16 11:04:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-//complete this code
-class Animal {}
+// Animal Class
+class Animal {
+  // Constructor accepts species for the animal
+  constructor(species) {
+    this.species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+// Dog Class inherits from Animal
+class Dog extends Animal {
+  // Constructor calls the parent constructor with species
+  constructor(species) {
+    super(species); // Call to Animal's constructor
+  }
+
+  // Method specific to Dog to bark
+  bark() {
+    console.log('woof');
+  }
+}
+
+// Cat Class inherits from Animal
+class Cat extends Animal {
+  // Constructor calls the parent constructor with species
+  constructor(species) {
+    super(species); // Call to Animal's constructor
+  }
+
+  // Method specific to Cat to purr
+  purr() {
+    console.log('purr');
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
